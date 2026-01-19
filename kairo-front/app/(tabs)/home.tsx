0a918091e341d97622fe407391f916596e904e52
kuczkoowska <jagoda.kucz@wp.pl>
2026-01-19 21:14:30 +0100

    add streak on top

--- kairo-front/app/(tabs)/home.tsx
+++ kairo-front/app/(tabs)/home.tsx
@@ -2,21 +2,33 @@ import {Text, View} from "react-native";
 import HabitList from "@/src/components/habit/HabitList";
 import {useRouter} from 'expo-router';
 import {homeScreenStyles, sharedFonts, sharedStyles} from "@/global";
+import {useAuth} from "@/src/contexts/AuthContext";
+import {Flame} from "@tamagui/lucide-icons";
 
 export default function Home() {
     const router = useRouter();
+    const {user, refreshProfile} = useAuth();
 
     return (
         <View style={[sharedStyles.basicContainer, {marginTop: 64}]}>
             <View style={homeScreenStyles.header}>
-                <Text style={[sharedFonts.headerText, {marginBottom: 4}]}>Today</Text>
-                <Text style={sharedFonts.mediumSubtleText}>
-                    {new Date().toLocaleDateString('en-US', {
-                        weekday: 'long',
-                        month: 'long',
-                        day: 'numeric'
-                    })}
-                </Text>
+                <View>
+                    <Text style={[sharedFonts.headerText, {marginBottom: 4}]}>Today</Text>
+                    <Text style={sharedFonts.mediumSubtleText}>
+                        {new Date().toLocaleDateString('en-US', {
+                            weekday: 'long',
+                            month: 'long',
+                            day: 'numeric'
+                        })}
+                    </Text>
+                </View>
+
+                <View style={homeScreenStyles.streakContainer}>
+                    <Flame size={24} color="#F59E0B" fill="#F59E0B"/>
+                    <Text style={homeScreenStyles.streakText}>
+                        {user?.streak ?? 0}
+                    </Text>
+                </View>
             </View>
             <HabitList onAdd={() => router.push('/habit/add')}
                        onEditHabit={(userHabitId: number) => {
@@ -27,4 +39,3 @@ export default function Home() {
         </View>
     );
 }
-
